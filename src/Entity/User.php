62b1834cfe8ba2fcaa96e1634f26b64f5d3e776c
Bloutime <schneider.myckael@gmail.com>
2021-07-22 14:11:38 +0200

    CRUD USER DONE

---
+++ src/Entity/User.php
@@ -0,0 +1,349 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @method string getUserIdentifier()
+ * @UniqueEntity(fields={"email"}, message="There is already an account with this email")
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $age;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $pays;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $adresse;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $ville;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private ?int $city;
+
+    /**
+     * @ORM\Column(type="array")
+     */
+    private array $roles = [];
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $profil_picture;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private ?string $description;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private ?int $rayon_action;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private ?int $note;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private ?bool $sitter_verifie;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private ?int $tarif;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isVerified = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getAge(): ?string
+    {
+        return $this->age;
+    }
+
+    public function setAge(string $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getPays(): ?string
+    {
+        return $this->pays;
+    }
+
+    public function setPays(string $pays): self
+    {
+        $this->pays = $pays;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->adresse;
+    }
+
+    public function setAdresse(string $adresse): self
+    {
+        $this->adresse = $adresse;
+
+        return $this;
+    }
+
+    public function getVille(): ?string
+    {
+        return $this->ville;
+    }
+
+    public function setVille(string $ville): self
+    {
+        $this->ville = $ville;
+
+        return $this;
+    }
+
+    public function getCity(): ?int
+    {
+        return $this->city;
+    }
+
+    public function setCity(int $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getRoles(): ?array
+    {
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+
+        return $this;
+    }
+
+    public function getProfilPicture(): ?string
+    {
+        return $this->profil_picture;
+    }
+
+    public function setProfilPicture(?string $profil_picture): self
+    {
+        $this->profil_picture = $profil_picture;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getRayonAction(): ?int
+    {
+        return $this->rayon_action;
+    }
+
+    public function setRayonAction(?int $rayon_action): self
+    {
+        $this->rayon_action = $rayon_action;
+
+        return $this;
+    }
+
+    public function getNote(): ?int
+    {
+        return $this->note;
+    }
+
+    public function setNote(?int $note): self
+    {
+        $this->note = $note;
+
+        return $this;
+    }
+
+    public function getSitterVerifie(): ?bool
+    {
+        return $this->sitter_verifie;
+    }
+
+    public function setSitterVerifie(?bool $sitter_verifie): self
+    {
+        $this->sitter_verifie = $sitter_verifie;
+
+        return $this;
+    }
+
+    public function getTarif(): ?int
+    {
+        return $this->tarif;
+    }
+
+    public function setTarif(?int $tarif): self
+    {
+        $this->tarif = $tarif;
+
+        return $this;
+    }
+
+    public function getSalt()
+    {
+        // TODO: Implement getSalt() method.
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function __call($name, $arguments)
+    {
+        // TODO: Implement @method string getUserIdentifier()
+    }
+
+    public function isVerified(): bool
+    {
+        return $this->isVerified;
+    }
+
+    public function setIsVerified(bool $isVerified): self
+    {
+        $this->isVerified = $isVerified;
+
+        return $this;
+    }
+}
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,154 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'required' => true,
+                'label'=>"Nom d'utilisateur",
+                'attr'=>[
+                    'placeholder' => "Nom d'utilisateur"
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'label'=>'Email',
+                'attr'=>[
+                    'placeholder' => "Adresse email"
+                ]
+            ])
+            ->add('nom', TextType::class, [
+                'required' => true,
+                'label'=>'Nom',
+                'attr'=>[
+                    'placeholder' => "Nom"
+                ]
+            ])
+            ->add('prenom', TextType::class, [
+                'required' => true,
+                'label'=>'Prénom',
+                'attr'=>[
+                    'placeholder' => "Prénom"
+                ]
+            ])
+            ->add('age', IntegerType::class, [
+                'required' => true,
+                'label'=>'Âge',
+                'attr'=>[
+                    'placeholder' => "Âge"
+                ]
+            ])
+            ->add('pays', TextType::class,[
+                'required' => true,
+                'label'=>'Pays',
+                'attr'=>[
+                    'placeholder' => "Pays"
+                ]
+            ])
+            ->add('adresse', TextType::class, [
+                'required' => true,
+                'label'=>'Adresse',
+                'attr'=>[
+                    'placeholder' => "Adresse"
+                ]
+            ])
+            ->add('ville', TextType::class, [
+                'required' => true,
+                'label'=>'Ville',
+                'attr'=>[
+                    'placeholder' => "Ville"
+                ]
+            ])
+            ->add('city', NumberType::class, [
+                'required' => true,
+                'label'=>'CodePostal',
+                'attr'=>[
+                    'placeholder' => "Code postal"
+                ]
+            ])
+            ->add('roles', ChoiceType::class ,[
+                'required' => true,
+                'choices' => array(
+                    'Sitter' => "ROLES_SITTER",
+                    'Master' => "ROLES_MASTER"
+                )
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'constraints' => [
+                    new IsTrue([
+                        'message' => 'You should agree to our terms.',
+                    ]),
+                ],
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
+                'options' => ['attr' => ['class' => 'password-field']],
+                'required' => true,
+                'first_options'  => [
+                    'label' => 'Password',
+                    'attr' => [
+                        'placeholder'=>"Mot de passe"
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Repeat Password',
+                    'attr' => [
+                        'placeholder'=> "Mot de passe"
+                    ]
+                ],
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Please enter a password',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($roleArray){
+                    return count($roleArray)? $roleArray[0]: null;
+                },
+                function ($roleString){
+                    return [$roleString];
+                }
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
